bb61e9e1f904bacaca6fdbafe4a6e843aec32216
Joon Kang <joonkang@monzo.com>
2022-12-09 08:33:34 +0000

    feat: Wrap with numba njit decorator

--- src/fwer/procedure.py
+++ src/fwer/procedure.py
@@ -1,10 +1,12 @@
 import numpy as np
 from numpy import typing as npt
 from src.fwer import adjusted_p as ap
+from numba import njit
 
 # TODO: Write documentatiaon
 
 
+@njit
 def bonferroni_procedure(
     p_vals: list[float], alpha: float = 0.05
 ) -> npt.NDArray[np.bool_]:
@@ -12,30 +14,33 @@ def bonferroni_procedure(
     return _p_vals < ap.bonferroni_alpha(m, alpha)
 
 
+@njit
 def sidak_procedure(p_vals: list[float], alpha: float = 0.05) -> npt.NDArray[np.bool_]:
     p, m = np.asarray(p_vals), len(p_vals)
     return p < ap.sidak_alpha(m, alpha)
 
 
+@njit
 def holm_step_down_procedure(
     p_vals: list[float], alpha: float = 0.05
 ) -> npt.NDArray[np.bool_]:
     _p_vals, m = np.asarray(p_vals), len(p_vals)
-    idx = np.argsort(_p_vals)
+    idx = _p_vals.argsort()
     test = _p_vals[idx] > ap.holm_hochberg_alpha(m, alpha)
     r = m - np.sum(test)  # Minimum ordered index k that suffices the test above
-    reject = np.zeros(len(_p_vals), dtype=bool)
+    reject = np.zeros(len(_p_vals), dtype=np.bool8)
     reject[idx[0:r]] = True
     return reject
 
 
+@njit
 def hochberg_step_up_procedure(
     p_vals: list[float], alpha: float = 0.05
 ) -> npt.NDArray[np.bool_]:
     _p_vals, m = np.asarray(p_vals), len(p_vals)
-    idx = np.argsort(_p_vals)
+    idx = _p_vals.argsort()
     test = _p_vals[idx] <= ap.holm_hochberg_alpha(m, alpha)
     r = np.sum(test)  # Largest ordered index k that suffices the test above
-    reject = np.zeros(len(_p_vals), dtype=bool)
+    reject = np.zeros(len(_p_vals), dtype=np.bool8)
     reject[idx[0:r]] = True
     return reject
